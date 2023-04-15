2a07c8e52624b9fe0f8708c2f620107782099ae5
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2023-04-16 00:00:15 +0200

    update account balance component

--- client/src/components/Tables/AccountBalanceTable.tsx
+++ client/src/components/Tables/AccountBalanceTable.tsx
@@ -1,22 +1,26 @@
 import { useEffect, useState } from 'react';
 
 import { getAccountBalance } from "../../services/api";
-import { Balance } from "../../../../shared/types/Account";
+import { AccountBalance } from "../../../../shared/types/Account";
 
 import styles from "./Table.module.css";
 
 export function AccountBalanceTable() {
 
-  const [accountBalance, setAccountBalance] = useState<Balance[]>([]);
+  const [accountBalance, setAccountBalance] = useState<AccountBalance>([]);
 
   useEffect(() => {
-    const fetchDataAsync = async () => {
-      const result = await getAccountBalance();
-      setAccountBalance(result);
-    };
+    const fetchData = async () => {
+      const response = await getAccountBalance();
+      setAccountBalance(response);
+    }
 
-    fetchDataAsync();
+    fetchData();
   }, []);
+
+  if (!accountBalance || accountBalance.length === 0) {
+    return <p>Loading...</p>
+  }
 
   return (
     <>
@@ -24,16 +28,26 @@ export function AccountBalanceTable() {
       <table className={styles.table}>
         <thead>
           <tr>
-            <th className={styles.th}>Asset</th>
+            <th className={styles.th}>Name</th>
+            <th className={styles.th}>Ticker[K]</th>
+            <th className={styles.th}>Ticker</th>
             <th className={styles.th}>Balance</th>
+            <th className={styles.th}>Price</th>
+            <th className={styles.th}>Value [EUR]</th>
+            <th className={styles.th}>Staking</th>
           </tr>
         </thead>
         <tbody>
-          {accountBalance && accountBalance.map((row, index) => {
+          {accountBalance.map((row, index) => {
             return (
               <tr key={index}>
-                <td className={styles.td}>{row.asset}</td>
+                <td className={styles.td}>{row.name}</td>
+                <td className={styles.td}>{row.krakenTicker}</td>
+                <td className={styles.td}>{row.ticker}</td>
                 <td className={styles.td}>{row.balance}</td>
+                <td className={styles.td}>{row.currentPrice}</td>
+                <td className={styles.td}>{row.priceEur}</td>
+                <td className={styles.td}>{row.isStaking ? 'Yes' : 'No'}</td>
               </tr>
             )
           })}
