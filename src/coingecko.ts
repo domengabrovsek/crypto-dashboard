97cd77189656fd225d59768a48f0581229498657
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2023-04-16 00:01:04 +0200

    extract coingecko mocks to file

--- src/coingecko.ts
+++ src/coingecko.ts
@@ -1,5 +1,6 @@
 import { appConfig } from './config/appConfig';
 import { Crypto, Fiat } from '../shared/constants/enums';
+import { getMockedCoinGeckoResponse } from './mocks/coingecko-mock';
 
 interface CoinData {
   id: string;
@@ -9,78 +10,12 @@ interface CoinData {
   lastUpdated: string;
 }
 
-export const getCurrentPrice = async (crypto: Crypto, fiat: Fiat = Fiat.EUR, test: boolean = true): Promise<CoinData> => {
+export const getTokenInfo = async (crypto: Crypto, fiat: Fiat = Fiat.EUR): Promise<CoinData> => {
 
-  if (test) {
+  const isTest = appConfig.get('IsTest');
 
-    switch (crypto) {
-      case Crypto.BTC:
-        return {
-          id: 'bitcoin',
-          symbol: 'btc',
-          name: 'Bitcoin',
-          price: 50000,
-          lastUpdated: '2021-05-01T00:00:00.000Z',
-        };
-      case Crypto.ETH:
-        return {
-          id: 'ethereum',
-          symbol: 'eth',
-          name: 'Ethereum',
-          price: 3000,
-          lastUpdated: '2021-05-01T00:00:00.000Z',
-        };
-      case Crypto.XLM:
-        return {
-          id: 'stellar',
-          symbol: 'xlm',
-          name: 'Stellar',
-          price: 0.5,
-          lastUpdated: '2021-05-01T00:00:00.000Z',
-        };
-      case Crypto.ADA:
-        return {
-          id: 'cardano',
-          symbol: 'ada',
-          name: 'Cardano',
-          price: 2,
-          lastUpdated: '2021-05-01T00:00:00.000Z',
-        };
-      case Crypto.DOT:
-        return {
-          id: 'polkadot',
-          symbol: 'dot',
-          name: 'Polkadot',
-          price: 40,
-          lastUpdated: '2021-05-01T00:00:00.000Z',
-        };
-      case Crypto.MATIC:
-        return {
-          id: 'polygon',
-          symbol: 'matic',
-          name: 'Polygon',
-          price: 2,
-          lastUpdated: '2021-05-01T00:00:00.000Z',
-        };
-      case Crypto.ZEC:
-        return {
-          id: 'zcash',
-          symbol: 'zec',
-          name: 'Zcash',
-          price: 200,
-          lastUpdated: '2021-05-01T00:00:00.000Z',
-        };
-      case Crypto.DOGE:
-        return {
-          id: 'dogecoin',
-          symbol: 'doge',
-          name: 'Dogecoin',
-          price: 0.5,
-          lastUpdated: '2021-05-01T00:00:00.000Z',
-        };
-      default:
-        throw new Error(`Unknown crypto: ${crypto}`);
-    }
+  if (isTest) {
+    return getMockedCoinGeckoResponse(crypto);
   }
 
   try {
